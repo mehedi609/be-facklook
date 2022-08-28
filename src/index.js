7624804289904345e8ffe626b56ff2440d38885b
mehediPC <mehedi609@gmail.com>
2022-08-29 03:12:44 +0600

    Initial setup

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('welcome from home');
+  res.send({ msg: 'welcome from home' });
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`server is listening on http://localhost:8000`);
+  console.log(`server is listening on http://localhost:${PORT}`);
 });
